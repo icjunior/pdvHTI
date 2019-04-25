ca14f907f1c1811ec78a528960acbdf21f8265c9
Ismael Junior <Ismael Junior@DESKTOP-6VF89IA>
2019-04-25 11:21:38 -0300

    Nota fiscal de saída

---
+++ src/br/com/htisoftware/pdv/bean/LancamentoNotaSaidaBean.java
@@ -0,0 +1,17 @@
+package br.com.htisoftware.pdv.bean;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class LancamentoNotaSaidaBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	public void emitir() {
+		
+	}
+}
--- src/br/com/htisoftware/pdv/enums/TipoCartao.java
+++ src/br/com/htisoftware/pdv/enums/TipoCartao.java
@@ -1,5 +1,5 @@
 package br.com.htisoftware.pdv.enums;
 
 public enum TipoCartao {
-	Débito, Crédito;
+	DEBITO, CREDITO;
 }
